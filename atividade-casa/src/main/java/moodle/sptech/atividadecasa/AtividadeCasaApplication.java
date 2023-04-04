9fc548c4082c7eefa01920ba13c7ec53942177a8
IvanCCO <ivan.miranda@sptech.school>
2023-04-03 21:44:28 -0300

    f

---
+++ atividade-casa/src/main/java/moodle/sptech/atividadecasa/AtividadeCasaApplication.java
@@ -0,0 +1,13 @@
+package moodle.sptech.atividadecasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeCasaApplication.class, args);
+	}
+
+}
